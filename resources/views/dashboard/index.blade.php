f8da7ddd240310f4965b88702fbcd4a3d37abe24
MIkhsanB546 <mikhsanb546@gmail.com>
2026-06-20 16:10:22 +0700

    feat: add layout and index

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="container-fluid">
+        <!--begin::Row-->
+        <div class="row">
+            <div class="col-sm-6">
+                <h3 class="mb-0">Selamat Datang!</h3>
+            </div>
+        </div>
+        <!--end::Row-->
+    </div>
+@endsection
